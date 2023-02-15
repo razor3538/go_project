f2de4df303fe71cb1f2879510253c9ed5d355b90
Гайфутдинов Эдуард Александрович <gayfutdinov_ea@w902147cash1.corp.tander.ru>
2023-02-16 02:14:50 +0300

    fix

--- internal/repository/order_repository.go
+++ internal/repository/order_repository.go
@@ -43,7 +43,7 @@ func (or *OrderRepo) GetByUser(userID string) ([]domain.Order, error) {
 		Order("created_at desc").
 		Scan(&orders).Error
 	if err != nil {
-		return []domain.Order, err
+		return []domain.Order{}, err
 	}
 	return orders, err
 }
