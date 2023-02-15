fd8f82d6e10f5e997f5289dbfc5c0c5d8b4fd274
Гайфутдинов Эдуард Александрович <gayfutdinov_ea@MacBook-Pro-Apple-2.local>
2023-02-16 02:59:06 +0300

    fix

--- internal/routes/order_api.go
+++ internal/routes/order_api.go
@@ -108,6 +108,11 @@ func (o *Order) Get(c *gin.Context) {
 	orderModel, err := orderService.GetAllByUser(id)
 
 	if err != nil {
+		println("err.Error()")
+		println(err.Error())
+		println(err.Error())
+		println("err.Error()")
+
 		tools.CreateError(http.StatusBadRequest, err, c)
 		return
 	}
