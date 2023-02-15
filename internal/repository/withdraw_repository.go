d4b810047a2927fc7f482d4fac565d2bddaa4e05
Гайфутдинов Эдуард Александрович <gayfutdinov_ea@MacBook-Pro-Apple-2.local>
2023-02-16 00:55:18 +0300

    fix

--- internal/repository/withdraw_repository.go
+++ internal/repository/withdraw_repository.go
@@ -38,6 +38,7 @@ func (wr *WithdrawRepo) Pay(withdrawal domain.Withdrawals) (domain.Withdrawals, 
 		Table("balances as b").
 		Where("b.user_id = ?", withdrawal.UserID).
 		Update("current", balance.Current-withdrawal.Sum).
+		Update("withdrawal", balance.Withdrawn+withdrawal.Sum).
 		Error; err != nil {
 		return domain.Withdrawals{}, err
 	}
