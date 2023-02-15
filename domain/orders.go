394b13217c24fd772293b42a052415665120f672
Гайфутдинов Эдуард Александрович <gayfutdinov_ea@MacBook-Pro-Apple-2.local>
2023-02-15 22:31:46 +0300

    fix

--- domain/orders.go
+++ domain/orders.go
@@ -8,5 +8,5 @@ type Order struct {
 	Number  string    `gorm:"type:text;unique" json:"number"`
 	UserID  uuid.UUID `gorm:"type:uuid" json:"user_id"`
 	Status  string    `gorm:"type:text" default:"NEW"`
-	Accrual string    `gorm:"type:float"`
+	Accrual string    `gorm:"type:text"`
 }
