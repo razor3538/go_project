73783c1b70cbde117b362f4e350326fc9c4465e2
Гайфутдинов Эдуард Александрович <gayfutdinov_ea@MacBook-Pro-Apple-2.local>
2023-02-20 11:16:21 +0300

    добавил более информативный вывод ошибки

--- config/postgres.go
+++ config/postgres.go
@@ -5,20 +5,15 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"os"
 )
 
 // DB is database instance
 var DB *gorm.DB
 
 func InitDB() {
-	//connectionString := fmt.Sprintf("host=localhost port=5432 user=postgres dbname=tander password=123 sslmode=disable")
-
-	// test
 	db, err := gorm.Open("postgres", Env.BdConnection)
-	//db, err := gorm.Open("postgres", connectionString)
 	if err != nil {
-		os.Exit(4)
+		panic(err)
 	}
 
 	DB = db
